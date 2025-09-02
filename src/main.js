b7ae193d6a7126e084a6bf982b4496c0f4cae7e0
Andrew Shtyka <andrii.shtyka@gmail.com>
2025-09-02 19:10:25 +0300

    update tip % input logic

--- src/main.js
+++ src/main.js
@@ -157,13 +157,14 @@ function initFunction() {
     let val = inputCustomTip.value;
     val = val.replace(",", ".");
 
-    if (/^0[1-9]/.test(val)) {
+    if (/^0[1-9]/.test(val)) {``
       inputCustomTip.value = val.replace(/^0/, "");
     }
     switchRadioAndInput(inputCustomTip);
     updateValues();
   });
 
+  inputPeople;
   inputPeople.addEventListener("input", () => {
     let val = inputPeople.value;
     val = val.replace(",", ".");
